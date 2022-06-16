2265126656d38ccc6162e650b7f1d8d624d4470c
haayd <haydee.sramirez@gmail.com>
2022-06-16 16:05:41 -0700

    export controller action newSkill as new

--- routes/skills.js
+++ routes/skills.js
@@ -4,7 +4,8 @@ import * as skillsCtrl from "../controllers/skills.js"
 const router = Router()
 
 /* GET users listing. */
-router.get('/', skillsCtrl.index) 
+router.get('/', skillsCtrl.index)
+router.get("/new", skillsCtrl.new)
 
 
 //create UI that will intitial this request- look at the CHART!!
